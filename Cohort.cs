ec429d3a5cd6af115796dcda56036105d41ac6c5
Jordan Castelloe <jordan.castelloe@gmail.com>
2019-04-30 11:10:17 -0400

    added demo code from class

--- Cohort.cs
+++ Cohort.cs
@@ -3,32 +3,41 @@ using System.Collections.Generic;
 
 namespace example_code
 {
-    class Cohort{
+    class Cohort
+    {
 
-        public Cohort(string nameParam){
+        public Cohort(string nameParam)
+        {
             Name = nameParam;
         }
-        public string Name {get;}
+        public string Name { get; }
 
         //This is an example of method overloading in C#
         // Two methods have the same name, but different types of parameters (aka "arities")
         // If we call enrollStudent with a parameter of type Student, it'll execute the code in the first method
-        public void enrollStudent(Student newStudent){
+
+        public void enrollStudent(Student newStudent)
+        {
             newStudent.CurrentCohort = this;
             Students.Add(newStudent);
         }
+
 
 
         // If we call enrollStudent and pass in a list of students, it will execute this code
         // This is an example of method-based polymorphism, which means "many forms"
         // Polymorphism is generally referred to the third pillar of object oriented programming, along with encapsulation and inheritance
         // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/polymorphism
-        public void enrollStudent(List<Student> newStudents){
-            foreach(Student currentStudent in newStudents){
+        public void enrollStudent(List<Student> newStudents)
+        {
+            foreach (Student currentStudent in newStudents)
+            {
                 enrollStudent(currentStudent);
             }
         }
 
-        public List<Student> Students {get; set;} = new List<Student>();
+
+
+        public List<Student> Students { get; set; } = new List<Student>();
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,29 +8,117 @@ namespace example_code
     {
         static void Main(string[] args)
         {
-            List<int> cohortStudentCount = new List<int>()
-            {
-                25, 12, 28, 22, 11, 25, 27, 24, 19
+            // List<int> cohortStudentCount = new List<int>()
+            // {
+            //     25, 12, 28, 22, 11, 25, 27, 24, 19
+            // };
+
+            // double avg = cohortStudentCount.Average();
+            // int sum = cohortStudentCount.Sum();
+            // IEnumerable<int> biggest = cohortStudentCount.OrderByDescending(count => count).Take(3);
+            // int smallest = cohortStudentCount.Min();
+
+            // List<int> doubledCohortSizes = cohortStudentCount.Select(studentCount => studentCount * 2).ToList();
+
+            // doubledCohortSizes.ForEach(n => Console.WriteLine($"Doubled number: {n}"));
+
+            // List<int> idealCohortSizes = cohortStudentCount.Where(count => count < 20 && count > 10).OrderBy(n => n).ToList();
+
+            // List<int> idealCohortSizes = (from count in cohortStudentCount where count < 20 && count > 10 orderby count select count).ToList();
+
+            // idealCohortSizes.ForEach(n => Console.WriteLine($"Ideal cohort size: {n}"));
+
+
+
+            //  Using only a `foreach` loop and basic arithmetic:
+
+            // 1. Find the average number of students per cohort.
+            // int sum = 0;
+            // int count = 0;
+            // int largestCohort = 0;
+
+            // foreach(int studentCount in cohortStudentCount){
+            //     sum += studentCount;
+            //     count ++;
+            //     if(studentCount > largestCohort){
+            //         largestCohort = studentCount;
+            //     }
+            // }
+
+            // int smallestCohort = largestCohort;
+            // foreach(int studentCount in cohortStudentCount){
+            //     if(studentCount < smallestCohort){
+            //         smallestCohort = studentCount;
+            //     }
+            // }
+
+            // Console.WriteLine(sum);
+            // int average = sum/count;
+            // Console.WriteLine(average);
+            // Console.WriteLine(smallestCohort);
+            // Console.WriteLine(largestCohort);
+            // 2. Find the total number of students who graduated in the past year.
+            // 3. How many students were in the biggest cohort?
+
+            // 4. How many students were in the smallest?
+
+            // ---------- Lightning Exercise 2 -------------- //
+
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+            List<string> LFruits = (from fruit in fruits where fruit[0].ToString() == "L" select fruit).Take(2).ToList();
+
+            // foreach(string fruit in LFruits){
+            //     Console.WriteLine(fruit);
+            // }
+
+            // LFruits.ForEach(fruit => Console.WriteLine(fruit));
+
+
+            // var LFruits = fruits.Where(f => f.Substring(0, 1) == "L");
+
+            // var LFruits = fruits.Where(f => f.StartsWith("L"));
+
+
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>(){
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
             };
 
-            /*
-                Figure out which cohorts fell within the range
-                of ideal sizes - between 15 and 25 students. Also, sort
-                the new enumerable collection in ascending order.
-            */
+            IEnumerable<int> fourSixMultiples = numbers.Where(num => num % 4 == 0 || num % 6 == 0);
 
-            // Smallest cohort? .Min()
+            IEnumerable<int> multiples = from number in numbers where number % 4 == 0 || number % 6 == 0 select number;
 
-            // Largest cohort? .Max()
+            // Order these student names alphabetically, in descending order (Z to A)
+            List<string> names = new List<string>()
+                {
+                    "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
+                    "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
+                    "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
+                    "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
+                    "Francisco", "Tre"
+                };
 
-            // Total number of students? .Sum()
+            List<string> descend = names.OrderByDescending(n => n).ToList();
 
-            // Average? .Average()
+            IEnumerable<string> anotherTry = from taco in names orderby taco descending select taco;
 
-            // .OrderBy() and .OrderByDescending()
+            // Build a collection of these numbers sorted in ascending order
+            List<int> nums = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            IEnumerable<int> ascending = nums.OrderBy(n => n);
+            IEnumerable<int> ascendingTwo = from num in nums orderby num select num;
 
 
-            Cohort one = new Cohort("Cohort One");
+
+
+
+            // --------- Create some instances of our classes ---------- //
+            Cohort one = new Cohort("Cohort 1");
             Cohort twentySix = new Cohort("Cohort 26");
 
 
@@ -46,9 +134,13 @@ namespace example_code
 
 
             // Previous Students
-            Student Shu = new Student("Shu", "Sajid", "MRI Tech", "C#");
-            Shu.monthsToHire = 2;
-            Shu.startingSalary = 50000;
+            Student Shu = new Student("Shu", "Sajid", "MRI Tech", "C#")
+            {
+                monthsToHire = 2,
+                startingSalary = 50000
+            };
+            // Shu.monthsToHire = 2;
+            // Shu.startingSalary = 50000;
 
             Student Natasha = new Student("Natasha", "Cox", "Teacher", "JavaScript");
             Natasha.monthsToHire = 0.25;
@@ -65,8 +157,16 @@ namespace example_code
 
             // We used method overriding so that the enrollStudent method can take either one student at a time, or a whole list of them
             one.enrollStudent(David);
+            one.enrollStudent(Nikki);
+            one.enrollStudent(Tommy);
+
+
+
+
+
 
             one.enrollStudent(new List<Student>() { Tommy, Nikki });
+
             twentySix.enrollStudent(new List<Student>() { Shu, Natasha, Leah, Seth });
 
             List<Cohort> allCohorts = new List<Cohort>() { one, twentySix };
@@ -75,35 +175,30 @@ namespace example_code
 
             // Let's get a list of the total number of students in each cohort, like the one we started out with
             // .Select() is the new .map()
-            List<int> numberOfStudents = allCohorts.Select(c => c.Students.Count()).ToList();
+            List<int> cohortSizes = allCohorts.Select(cohort => cohort.Students.Count).ToList();
+            cohortSizes.ForEach(c => Console.WriteLine(c));
 
-            numberOfStudents.ForEach(i => Console.WriteLine(i));
+            // Which students in Cohort One like C# best?
+            // .Where() is the new .filter()
 
-            // Which students in Cohort one like C# best?
+            var cSharpStudents = one.Students.Where(singleStudent => singleStudent.FavoriteLanguage == "C#");
 
-            // IEnumerable<Student> cSharpStudents = from student in one.Students where student.FavoriteLanguage=="C#" select student;
+            IEnumerable<Student> cSharpStudents2 = from student in one.Students where student.FavoriteLanguage == "C#" select student;
 
-            IEnumerable<Student> cSharpStudents = one.Students.Where(s => s.FavoriteLanguage == "C#");
-
+            // TODO: Get all the students who like C# from all available cohorts?
 
             // Let's look at a report of how many students in cohort one favor different languages in cohort one
             // JavaScript - 2
             // C# - 1
-            IEnumerable<Dictionary<string, int>> studentReport = from student in one.Students
-                                                                 group student by student.FavoriteLanguage into
-                                                                 // languageGroup is an IGrouping<string, Student>
-                                                                 languageGroup
-                                                                 select new Dictionary<string, int>(){
+            var studentReport = from student in one.Students
+                                group student by student.FavoriteLanguage into
+                                // languageGroup is an IGrouping<string, Student>
+                                languageGroup
+                                select new Dictionary<string, int>(){
                 {languageGroup.Key, languageGroup.Count()}
             };
 
-            foreach (Dictionary<string, int> reportEntry in studentReport)
-            {
-                foreach (KeyValuePair<string, int> reportLineItem in reportEntry)
-                {
-                    Console.WriteLine($"{reportLineItem.Key} - {reportLineItem.Value}");
-                }
-            }
+
 
             // Build a report for each cohort in the allCohorts List
             // Use the ReportItem class
@@ -115,6 +210,15 @@ namespace example_code
                                                        averageMonthsToHire = cohort.Students.Select(s => s.monthsToHire).Average(),
                                                        averageStartingSalary = cohort.Students.Select(s => s.startingSalary).Average()
                                                    };
+            foreach (ReportItem item in cohortReport)
+            {
+                Console.WriteLine("----------");
+                Console.WriteLine(item.CohortName);
+                Console.WriteLine(item.averageMonthsToHire);
+                Console.WriteLine(item.averageStartingSalary);
+                Console.WriteLine(item.numberOfStudents);
+
+            }
 
 
 
